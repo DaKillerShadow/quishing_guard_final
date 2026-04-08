3610a56af925a54fac8c6f46e40446b3cfbc7523
mohamed abdelfattah <96657615+DaKillerShadow@users.noreply.github.com>
2026-04-08 03:44:32 +0200

    Create vercel-build.sh

---
+++ flutter_app/vercel-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 1. Clone ONLY the latest Flutter files to save Vercel disk space
+git clone https://github.com/flutter/flutter.git -b stable --depth 1 f
+
+# 2. Tell Vercel where Flutter is located
+export PATH="$PATH:$(pwd)/f/bin"
+
+# 3. Pre-download web artifacts to prevent Exit 64
+flutter precache --web
+
+# 4. Build the app using HTML renderer for Vercel stability
+flutter build web --release --web-renderer html --dart-define=API_BASE_URL=$API_BASE_URL
+
