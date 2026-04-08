e4acb983c5afd5ead103e223be242bdeba7410b8
mohamed abdelfattah <96657615+DaKillerShadow@users.noreply.github.com>
2026-04-08 10:58:25 +0200

    Update settings_screen.dart

--- flutter_app/lib/features/settings/settings_screen.dart
+++ flutter_app/lib/features/settings/settings_screen.dart
@@ -1,3 +1,5 @@
+// ADD THIS LINE AT THE TOP:
+import '../../core/services/history_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
