c36e11ac58b5e0d4eb6865c39712b924591deb4b
mohamed abdelfattah <96657615+DaKillerShadow@users.noreply.github.com>
2026-04-08 13:00:10 +0200

    Delete backend directory

--- backend/app/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-# This file should only export your Blueprints
-from .auth import bp as auth_bp
-from .analyse import bp as analyse_bp
-from .report import bp as report_bp
-# DO NOT import scorer here!
