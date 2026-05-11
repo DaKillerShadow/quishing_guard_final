559e6a33e4f21eec62bd123c7e689743c001d666
mohamed abdelfattah <96657615+DaKillerShadow@users.noreply.github.com>
2026-05-11 19:07:30 +0300

    Add health and admin blueprints to routes

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,5 +1,8 @@
 # This file should only export your Blueprints
-from .auth import bp as auth_bp
-from .analyse import bp as analyse_bp
-from .report import bp as report_bp
+from .auth       import bp as auth_bp
+from .analyse    import bp as analyse_bp
+from .report     import bp as report_bp
+from .health     import bp as health_bp
+from .admin      import bp as admin_bp
+from .scan_image import bp as scan_image_bp
 # DO NOT import scorer here!
